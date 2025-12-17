642475798a33e5da7e84f822d81193e76d071981
christanaomi <christanaomisilalahi@gmail.com>
2025-12-17 17:19:45 +0700

    Pair07
    Sukses

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * TEMPLATE Main.java
+ * Students must replace this file with their exported Java code from Flowgorithm.
+ * For testing, you can implement a simple echo or example solution.
+ */
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // placeholder to avoid compilation error if student doesn't provide code
+        // This program does nothing by default. Replace with exported code from Flowgorithm.
+        Scanner sc = new Scanner(System.in);
+        // read all input and exit
+        while (sc.hasNextLine()) {
+            sc.nextLine();
+        }
+    }
+}
